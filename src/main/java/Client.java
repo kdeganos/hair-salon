d7373de6f8e8fe21a692903d65e97beb39f63ef3
Kevin Deganos <kdeganos-09@sandiego.edu>
2016-05-06 09:38:52 -0700

    pass all Stylist equivalent tests for new Client class

---
+++ src/main/java/Client.java
@@ -0,0 +1,62 @@
+import java.util.List;
+import org.sql2o.*;
+
+public class Client{
+  private String name;
+  private int id;
+  private int stylist_id;
+
+  public Client (String name, int stylist_id) {
+    this.name = name;
+    this.stylist_id = stylist_id;
+   }
+
+  public String getName() {
+     return name;
+   }
+
+  public int getId() {
+     return id;
+  }
+
+  public int getStylistId() {
+    return stylist_id;
+  }
+
+  public static List<Client> all() {
+    String sql = "SELECT * FROM clients";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Client.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object client) {
+    if (!(client instanceof Client)) {
+      return false;
+    } else {
+      Client otherClient = (Client) client;
+      return this.getName().equals(otherClient.getName());
+    }
+  }
+
+  public void save() {
+    String sql = "INSERT INTO clients (name, stylist_id) VALUES (:name, :stylist_id)";
+    try(Connection con = DB.sql2o.open()) {
+      this.id = (int) con.createQuery(sql, true)
+                .addParameter("name", this.name)
+                .addParameter("stylist_id", this.stylist_id)
+                .executeUpdate()
+                .getKey();
+    }
+  }
+
+  public static Client find(int id) {
+    String sql = "SELECT * FROM clients WHERE id=:id";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql)
+            .addParameter("id", id)
+            .executeAndFetchFirst(Client.class);
+    }
+  }
+}
---
+++ src/test/java/ClientTest.java
@@ -0,0 +1,57 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+
+
+public class ClientTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void client_instantiatesCorrectly() {
+    Client client = new Client("Name", 1);
+
+    assertEquals(true, client instanceof Client);
+  }
+
+  @Test
+  public void getName_returnsName_Name() {
+    Client client = new Client("Name", 1);
+
+    assertEquals("Name", client.getName());
+  }
+
+  @Test
+  public void all_initiallyEmpty_0() {
+    Client client = new Client("Name", 1);
+
+    assertEquals(0, Client.all().size());
+  }
+
+  @Test
+  public void equals_returnsTrueIfBothNamesSame_true() {
+    Client client1 = new Client("Name", 1);
+    Client client2 = new Client("Name", 1);
+
+    assertTrue(client1.equals(client2));
+  }
+
+  @Test
+  public void save_savesInstanceToDBWithId() {
+    Client client = new Client("Name", 1);
+    client.save();
+    Client savedClient = Client.all().get(0);
+
+    assertEquals(client.getId(), savedClient.getId());
+  }
+
+  @Test
+  public void find_returnsCorrectInstanceOfClient() {
+    Client client = new Client("Name", 1);
+    client.save();
+    Client foundClient = Client.find(client.getId());
+
+    assertTrue(client.equals(foundClient));
+  }
+}
